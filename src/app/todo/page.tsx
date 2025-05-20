a3ec91c725a944664b4a8350762a619a74062212
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-20 11:40:38 +0330

    update

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -2,10 +2,6 @@ import { getTodo } from "@/lib/api";
 export default async function TodoPage() {
     const todos = await getTodo();
   
-    if (!todos || todos.length === 0) {
-      return <div className="text-red-500 text-center py-10">Not found TODO</div>;
-    }
-  
     return (
       <ul>
         {todos.map((todo) => (
