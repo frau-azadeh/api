bc0838555322a2d14e3d18a90761b6174d015864
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-26 10:53:57 +0330

    update page with Button components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+import Button from "@/components/ui/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div >
+    <div className="">
       <h1>Api Project</h1>
+      <Link href="/posts">
+        <Button variant="danger" size="md"> Go to post page</Button>
+      </Link>
+      <Link href="/photo">
+        <Button variant="primary" size="md">Go to photo page</Button>
+      </Link>
     </div>
   );
 }
