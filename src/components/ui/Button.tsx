13afb706097c4137c0e8dd5a66d6acae5c4f5309
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-27 14:20:25 +0330

    refactor css Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,12 @@
-import React, { ButtonHTMLAttributes, ReactNode } from "react";
-import clsx from "clsx";
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+import clsx from 'clsx';
+import React, { ButtonHTMLAttributes, ReactNode } from 'react'
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
   variant?: "primary" | "secondary" | "danger";
   size?: "xs" | "md" | "lg";
   children: ReactNode;
 }
+
 const Button: React.FC<ButtonProps> = ({
   className,
   variant = "primary",
@@ -12,10 +14,11 @@ const Button: React.FC<ButtonProps> = ({
   children,
   ...props
 }) => {
+
   const variantClass = {
-    primary: "bg-blue-500 text-white",
-    secondary: "bg-gray-500 text-white",
-    danger: "bg-red-500 text-white",
+    primary: "bg-blue-500",
+    secondary: "bg-gray-500",
+    danger: "bg-red-500",
   };
 
   const sizeClass = {
@@ -23,19 +26,20 @@ const Button: React.FC<ButtonProps> = ({
     md: "px-4 py-2 text-base",
     lg: "px-6 py-3 text-lg",
   };
+
   return (
     <button
-      {...props}
       className={clsx(
-        "px-2 py-4 rounded-lg cursor-pointer",
+        "rounded-lg cursor-pointer text-white",
         variantClass[variant],
         sizeClass[size],
-        className,
+        className
       )}
+      {...props}
     >
       {children}
     </button>
-  );
-};
+  )
+}
 
-export default Button;
+export default Button
