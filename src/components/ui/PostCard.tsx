4573b9b79d4e8f2fe142f69304a591753216a746
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-05-28 17:33:00 +0330

    style(PostCard): change css

--- src/components/ui/PostCard.tsx
+++ src/components/ui/PostCard.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
-import React from 'react'
-import { ChevronsRight } from "lucide-react"
+import Link from "next/link";
+import React from "react";
+import { ChevronsRight } from "lucide-react";
 interface PostCardProps {
   id?: number;
   title: string;
@@ -12,22 +12,26 @@ const PostCard: React.FC<PostCardProps> = ({
   id,
   title,
   body,
-  hideLink = false
+  hideLink = false,
 }) => {
   return (
-    <div className='border border-gray-300 rounded-xl p-4 bg-white shadow-md hover:shadow-xl transition duration-300'>
-      <h2 className='text-blue-400 line-clamp-1 mb-4 font-bold text-lg'>{title}</h2>
-      <p className='pl-2 text-justify text-base leading-8 line-clamp-4 mb-2 text-gray-600'>{body}</p>
-      {!hideLink && id !== undefined &&(
-        <Link 
+    <div className="border border-gray-300 rounded-xl p-4 bg-white shadow-md hover:shadow-xl transition duration-300">
+      <h2 className="text-blue-400 line-clamp-1 mb-4 font-bold text-lg">
+        {title}
+      </h2>
+      <p className="pl-2 text-justify text-base leading-8 line-clamp-4 mb-2 text-gray-600">
+        {body}
+      </p>
+      {!hideLink && id !== undefined && (
+        <Link
           href={`/posts/${id}`}
-          className='text-blue-500 hover:text-blue-800 duration-300 transition flex items-end gap-6'
-          >
-          Read more <ChevronsRight className='w-4 h-4'/>
+          className="text-blue-500 hover:text-blue-800 duration-300 transition flex items-end gap-6"
+        >
+          Read more <ChevronsRight className="w-4 h-4" />
         </Link>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default PostCard
+export default PostCard;
