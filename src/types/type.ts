3910186732d7f5c01aa45e6764a854d7a53ad90b
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-03 11:50:13 +0330

    add type UserPage

--- src/types/type.ts
+++ src/types/type.ts
@@ -15,3 +15,33 @@ export type TodoPage = {
   title: string;
   completed: string;
 };
+
+export interface Address{
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+}
+
+export interface Geo{
+  lat: string;
+  lang: string;
+}
+
+export interface Company{
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface UserPage{
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  geo: Geo;
+  phone: string;
+  website: string;
+  company: Company;
+}
