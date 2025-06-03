cce428e89571fff63a4315276b7e2421e6c8f99c
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-03 14:39:48 +0330

    add back button

---
+++ src/components/ui/GoBack.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { ChevronLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+const GoBack = () => {
+    const router = useRouter()
+  return (
+    <div className='flex  items-end'>
+    <button
+        onClick={()=>router.back()}
+        className='text-blue-500 hover:text-blue-950 font-bold m-2 flex items-end'
+    >
+        Go Back <ChevronLeft className='h-5 w-5'/>
+    </button>
+    </div>
+
+)
+}
+
+export default GoBack
