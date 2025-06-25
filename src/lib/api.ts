f7e18c2781e01c63b1c4096a0cf1bbce3dac06ca
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-25 20:48:29 +0330

    add post api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,68 +1,37 @@
-import { PhotoPage, PostPage, TodoPage, UserPage } from "@/types/type";
+import { PostPage } from "@/types/type";
 import axios, { AxiosError } from "axios";
 
 const jsonApi = axios.create({
-  baseURL: "https://jsonplaceholder.typicode.com",
-  timeout: 5000,
-});
+  baseURL:"https://jsonplaceholder.typicode.com",
+  timeout: 5000
+})
 
 const picsumApi = axios.create({
   baseURL: "https://picsum.photos",
-  timeout: 5000,
-});
+  timeout: 5000
+})
 
 export async function getPost(): Promise<PostPage[]> {
-  const res = await jsonApi.get<PostPage[]>("/posts");
-  return res.data;
+  const res = await jsonApi.get<PostPage[]>("/posts")
+  return res.data
 }
 
 export async function getPostById(id: string): Promise<PostPage | null> {
-  const postId = Number(id);
-  if (isNaN(postId)) return null;
+  const postId = Number(id)
+  if(isNaN(postId)) return null
 
   try {
-    const res = await jsonApi.get<PostPage>(`/posts/${postId}`);
-    return res.data;
+    const res = await jsonApi.get<PostPage>(`/posts/${postId}`)
+    return res.data
+
   } catch (error) {
-    const err = error as AxiosError;
-    if (err.response?.status === 404) {
-      console.warn(`This id=${postId} not found`);
-      return null;
+    const err = error as AxiosError
+    if(err.response?.status===404){
+      console.warn(`This id=${postId} not found`)
+      return null
     }
-    console.error("Error with get post", err.message);
-    return null;
+    console.error("This post error is:", err.message)
+    return null
   }
 }
 
-export async function getTodo(): Promise<TodoPage[]> {
-  const res = await jsonApi.get<TodoPage[]>("/todos");
-  return res.data;
-}
-
-export async function getTodoById(id: string): Promise<TodoPage | null> {
-  const todoId = Number(id);
-  if (isNaN(todoId)) return null;
-
-  try {
-    const res = await jsonApi.get<TodoPage>(`/todos/${todoId}`);
-    return res.data;
-  } catch (error) {
-    const err = error as AxiosError;
-    if (err.response?.status === 404) {
-      console.warn(`This id=${todoId} not found`);
-      return null;
-    }
-    console.error("error with get todo", err.message);
-    return null;
-  }
-}
-
-export async function getUser(): Promise<UserPage[]> {
-  const res = await jsonApi.get<UserPage[]>("/users");
-  return res.data;
-}
-
-export async function getPhoto(): Promise<PhotoPage[]> {
-  const res = await picsumApi.get<PhotoPage[]>("/v2/list");
-  return res.data;
-}
