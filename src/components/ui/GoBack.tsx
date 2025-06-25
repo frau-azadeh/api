ce3ae3485d69de605725f8d71bf90537ded32541
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-06-25 17:13:54 +0330

    refactor GoBack component

--- src/components/ui/GoBack.tsx
+++ src/components/ui/GoBack.tsx
@@ -1,20 +1,20 @@
-"use client";
-import { ChevronLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
-import React from "react";
+"use client"
+import { ChevronLeft } from 'lucide-react';
+import { useRouter } from 'next/navigation'
+import React from 'react'
 
 const GoBack = () => {
   const router = useRouter();
   return (
-    <div className="flex  items-end">
+    <div className='flex items-end'>
       <button
-        onClick={() => router.back()}
-        className="text-blue-500 hover:text-blue-950 font-bold m-2 flex items-end"
+        onClick={()=>router.back()}
+        className='text-blue-500 hover:text-blue-900 m-2 flex items-end '
       >
-        Go Back <ChevronLeft className="h-5 w-5" />
+        Go Back <ChevronLeft className='h-5 w-5'/>
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default GoBack;
+export default GoBack
