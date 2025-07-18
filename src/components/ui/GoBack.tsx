caa7183043d4e25ea36e25fba672a8acca94c0d3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-18 17:06:46 +0330

    delete Button Component

--- src/components/ui/GoBack.tsx
+++ src/components/ui/GoBack.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useRouter } from 'next/navigation'
 import React from 'react'
-import Button from './Button';
 import { ChevronLeft } from 'lucide-react';
 
 const GoBack:React.FC = () => {
