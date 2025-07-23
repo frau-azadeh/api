fab0a505e373c2c90aff458dc2cbdf40d2f3596a
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-23 12:10:34 +0330

    update: class button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,17 @@
-import clsx from 'clsx';
-import React, { ButtonHTMLAttributes, ReactNode } from 'react'
+import clsx from "clsx";
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "danger" | "call";
-  size?: "xs" | "md" | "lg" ;
+  size?: "xs" | "md" | "lg";
   children: ReactNode;
   icon?: ReactNode;
   iconPosition?: "right" | "left";
 }
 
-const Button:React.FC<ButtonProps> = ({
-  variant="primary",
-  size="xs",
+const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  size = "xs",
   children,
   className,
   icon,
@@ -23,19 +23,19 @@ const Button:React.FC<ButtonProps> = ({
     secondary: "bg-gray-500 hover:bg-gray-700 transition duration-300",
     danger: "bg-red-500 hover:bg-red-700 transition duration-300",
     call: "bg-green-500 hover:bg-green-700 transition duration-300",
-  }
+  };
 
   const sizeClass = {
     xs: "px-2 py-1 text-sm",
     md: "px-4 py-2 text-base",
     lg: "px-6 py-3 text-lg",
-  }
+  };
 
   return (
     <button
       {...props}
       className={clsx(
-        "rounded-lg text-white cursor-pointer",
+        "rounded-lg text-white cursor-pointer flex items-center",
         variantClass[variant],
         sizeClass[size],
         className,
@@ -43,7 +43,7 @@ const Button:React.FC<ButtonProps> = ({
     >
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
