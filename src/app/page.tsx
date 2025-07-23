4e341293680b83a7b5377b178726cce247a331d4
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-23 15:51:07 +0330

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,32 @@
 import Button from "@/components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
-  return (
+export default function Home(){
+  return(
     <div className="flex justify-center items-center h-screen">
       <div>
-        <div className="flex justify-center items-center mb-4">
-          <h1 className="text-blue-950 font-bold text-lg">Api Project</h1>
+        <div className="justify-center items-center flex mb-5">
+          <h1 className="text-blue-950 font-bold text-lg">API PROJECT</h1>
         </div>
-        <div className="flex justify-center items-center space-x-4">
-          <Link href="/posts">
-            <Button variant="danger" size="md">
+        <div className="justify-center items-center flex gap-10">
+          <Link href="./posts">
+            <Button
+              variant="primary"
+              size="lg"
+            >
               Go to post page
             </Button>
           </Link>
-          <Link href="/photo">
-            <Button variant="primary" size="md">
+          <Link href="./photo">
+            <Button 
+              variant="danger"
+              size="lg"
+            >
               Go to photo page
             </Button>
           </Link>
         </div>
       </div>
     </div>
-  );
+  )
 }
