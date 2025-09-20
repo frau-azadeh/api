ba4f4a02c8c76a6ce6a58dd99f6c736335d096a1
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-09-20 13:07:57 +0330

    update prettier

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,10 @@
-"use client"
-import type { ReactNode } from "react";
+"use client";
 
+import type { ReactNode } from "react";
 
 export default function ErrorPage(): ReactNode {
   return (
-    <div >
+    <div>
       <h1>Oops! Something Went Wrong...</h1>
       <a href="/">Go to Home Page</a>
     </div>
